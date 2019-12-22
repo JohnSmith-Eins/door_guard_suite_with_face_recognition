b0b9347fafa534f23571dc8a516e6a82247a2cef
風音瑠璃子 <starlightplan@live.com>
2019-12-23 00:16:45 +0800

    Delete gpio.py

--- gpio.py
+++
@@ -1,8 +0,0 @@
-import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3, GPIO.OUT, initial=GPIO.LOW)
-GPIO.output(3,GPIO.HIGH)
-time.sleep(5)
-GPIO.output(3,GPIO.LOW)
-GPIO.cleanup()
